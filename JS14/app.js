84a8ca68ed9175439a4034952b12af2661ae16df
Haya <hayaa.official10@gmail.com>
2024-10-11 22:36:35 +0500

    Update!!

--- JS14/app.js
+++ JS14/app.js
@@ -39,3 +39,92 @@ var user = {
 
   console.log('firstName' in user); 
   console.log('lastName' in user); 
+
+
+// 3:Create a constructor function with some properties. Now
+// create multiple records using the constructor.
+
+function Person(name, age, job) {
+  this.name = name;  
+  this.age = age;     
+  this.job = job;   
+
+  this.displayInfo = function() {
+      console.log("Name: " + this.name + ", Age: " + this.age + ", Job: " + this.job);
+  };
+}
+
+let person1 = new Person("Alice", 28, "Engineer");
+let person2 = new Person("Bob", 35, "Designer");
+let person3 = new Person("Charlie", 22, "Developer");
+
+
+person1.displayInfo(); 
+person2.displayInfo();  
+person3.displayInfo(); 
+
+
+//4: Suppose you want to check population of your area, their
+// educations and professions.
+// Create a constructor function which holds following
+// properties:
+// Name, gender, address, education, profession,
+// Enter all records one by one.
+// Hint:
+
+//  use select box for education and profession,
+//  use radio box for gender
+
+// Bonus : use can use localStorage to save records.
+
+function Person(name, gender, address, education, profession) {
+  this.name = name;
+  this.gender = gender;
+  this.address = address;
+  this.education = education;
+  this.profession = profession;
+}
+
+
+function getRecords() {
+  let records = localStorage.getItem('populationRecords');
+  return records ? JSON.parse(records) : [];
+}
+
+function saveRecords(records) {
+  localStorage.setItem('populationRecords', JSON.stringify(records));
+}
+
+function displayRecords() {
+  let records = getRecords();
+  let recordList = document.getElementById('recordList');
+  recordList.innerHTML = ''; 
+
+  records.forEach(record => {
+      let li = document.createElement('li');
+      li.textContent = `Name: ${record.name}, Gender: ${record.gender}, Address: ${record.address}, Education: ${record.education}, Profession: ${record.profession}`;
+      recordList.appendChild(li);
+  });
+}
+
+document.getElementById('populationForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+
+  let name = document.getElementById('name').value;
+  let gender = document.querySelector('input[name="gender"]:checked').value;
+  let address = document.getElementById('address').value;
+  let education = document.getElementById('education').value;
+  let profession = document.getElementById('profession').value;
+
+  let newPerson = new Person(name, gender, address, education, profession);
+
+  let records = getRecords();
+  records.push(newPerson);
+  saveRecords(records);
+
+  this.reset();
+  displayRecords();
+});
+
+displayRecords();
